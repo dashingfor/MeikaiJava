ae207dddb11c9755ded2d1e177cfce2181b403a5
dashingfor <124754005+dashingfor@users.noreply.github.com>
2024-05-15 17:48:53 +0800

    Create Practice9-5PeriodTester.java

---
+++ Practice9-5PeriodTester.java
@@ -0,0 +1,7 @@
+public class PeriodTester {
+    public static void main(String[] args) {
+        Period normal = new Period(new Day(2024, 5, 12), new Day(2024, 5, 18), "深色", "量少", "结块");
+
+        System.out.println(normal);
+    }
+}
